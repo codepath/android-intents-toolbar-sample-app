a4621fe881076f118609d313d753e2f1d2923e17
Slava Panasenko <usverster@gmail.com>
2019-08-17 18:22:27 -0700

    Migrate to AndroidX, switch to version vars; remove empty tests

--- app/src/main/java/com/codepath/jlin/navigationdemo/SearchActivity.java
+++ app/src/main/java/com/codepath/jlin/navigationdemo/SearchActivity.java
@@ -1,13 +1,11 @@
 package com.codepath.jlin.navigationdemo;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
